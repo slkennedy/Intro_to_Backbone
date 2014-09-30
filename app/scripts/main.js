9a2a884b7b701cf237cb978b01d5445a94393c20
slkennedy <slkennedy@gmail.com>
2014-09-30 15:33:29 -0400

    Adds list, list items and search bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,72 @@
 'use strict';
+
+var traveling = [
+    {name: "Ecuador" },
+    {name: "Japan" },
+    {name: "Jamaica" },
+    {name: "Italy" }
+];
+
+$('body').append("<div class='wrapper'></div>");
+// $('.wrapper').prepend("<input class='searchbar' type='search'></input>");
+
+var ListView = Backbone.View.extend({
+    tagName: 'ul',
+    className: 'countries',
+
+	render: function () {
+		$('.wrapper').append(this.el);
+	}
+});
+
+var ItemView = Backbone.View.extend({
+	tagName: 'li',
+	className: 'country',
+
+	initialize: function () {
+	},
+
+	render: function (options) {
+		this.name = options.name;
+		this.$el.text(this.name);
+		$('ul').append(this.el);
+	},
+
+});
+
+var SearchView = Backbone.View.extend ({
+	tagName:'input',
+	attributes:{'type':'search', 'placeholder':'Search For Stuffs'},
+	className:'searchbar',
+	
+	initialize: function () {
+		this.$el.on('change', this.filter)
+	},
+
+	filter: function(){
+		var b = _.filter(traveling, function(country) {
+			return country.name === $('.searchbar').val();
+		});
+	},
+	
+	render: function() {
+		$('.wrapper').prepend(this.el);
+	}
+});
+
+
+var listView = new ListView();
+listView.render();
+
+var searchView = new SearchView();
+searchView.render();
+
+_.each(traveling, function(travel) {
+	var itemView = new ItemView();
+	itemView.render(travel);
+});
+
+
+
+
+
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,16 +1,39 @@
-/* global describe, it */
-
 (function () {
     'use strict';
 
-    describe('List Test', function () {
-        it('should have a list', function () {
-        	expect(1).to.equal(2);
-        });
-
-        it('should have at least 1 list item', function (){
-        	expect(1).to.equal(2);
-
-        });
+	describe('Array of Objects Test', function () {
+        it('should be an array of objects', function () {
+        	expect(traveling).to.be.an('array');
+        	expect(traveling[0]).to.be.an('object');
+       	});
     });
+
+    describe('List Tests', function () {
+        it('should add a list to the DOM', function () {
+        	expect($('ul.countries').length).equal(1);
+        });
+
+        it('should have multiple items on the list', function () {
+        	expect($('li.country').length).to.equal(4);
+        });
+
+    });
+
+    describe('Container Test', function (){
+    	it('should have a container with a class of wrapper', function (){
+    		expect($('.wrapper')).to.be.ok;
+    	});
+    });
+
+    describe('Searchbar Test', function (){
+    	it('should have an input field', function (){
+    		expect($('input[type=search]')).to.be.ok;
+    	});
+    });
+
+    describe('Filter Test', function (){
+    	it('should update the number of list items being displayed when search bar is used', function (){
+    		expect($('li.country').length).to.not.equal(4);
+    	});
+    })
 })();
